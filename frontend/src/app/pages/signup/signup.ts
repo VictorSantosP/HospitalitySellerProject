a6067be6970a32240d4a6a5e8109d05afe7459f7
Victor Santos <vhdboss@gmail.com>
2026-09-21 22:38:12 -0300

    feat: signup page changes: created signup page

--- frontend/src/app/pages/signup/signup.ts
+++ frontend/src/app/pages/signup/signup.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayout } from '../../components/default-login-layout/default-login-layout';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { DefaultFormInput } from '../../components/default-form-input/default-form-input';
 
 @Component({
   imports: [
-    DefaultLoginLayout
+    DefaultLoginLayout,
+    ReactiveFormsModule,
+    DefaultFormInput
   ],
   selector: 'app-signup',
   styleUrl: './signup.css',
   templateUrl: './signup.html',
 })
-export class Signup {}
+export class Signup {
+  signupForm!: FormGroup;
+
+  constructor(){
+    this.signupForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.min(6)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+}
