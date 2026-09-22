40bd8a4ae15c277a92e01c0bb19b558a56674831
Victor Santos <vhdboss@gmail.com>
2026-09-21 22:19:58 -0300

    feat: login page changes: fixed the foward ref error that bug the input, and finished de stylization of the page at all

--- frontend/src/app/components/default-form-input/default-form-input.ts
+++ frontend/src/app/components/default-form-input/default-form-input.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 type InputTypes = "text" | "email" | "password"
 
@@ -7,13 +7,44 @@ type InputTypes = "text" | "email" | "password"
   imports: [
     ReactiveFormsModule
   ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DefaultFormInput),
+      multi: true
+    }
+  ],
   selector: 'app-default-form-input',
   styleUrl: './default-form-input.css',
   templateUrl: './default-form-input.html',
 })
-export class DefaultFormInput {
+export class DefaultFormInput implements ControlValueAccessor{
   @Input() type: InputTypes = "text";
-  @Input() formName: string = "";
   @Input() placeholder: string = "";
   @Input() label: string = "";
+  @Input() inputName: string = "";
+
+
+  value: string = "";
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.onChange(value);
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {}
 }
